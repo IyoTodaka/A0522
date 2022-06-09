5b1e12e8e650d5746b9c74e4ec275441dea5c33a
Iyo Todaka <iyo.todaka@gmail.com>
2022-06-09 02:05:48 -0700

    Java commit

---
+++ AFPS-w4d2/App.java
@@ -0,0 +1,9 @@
+import java.nio.ShortBuffer;
+
+public class App {
+
+    public static void main(String[] args)throws Exception{
+        System.out.println ("Heloo,World");
+        System.out.printf("%S","abcd");
+    }
+}
