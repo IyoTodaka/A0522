92192b51126c16fe944dad5cee2113f82373224e
Iyo Todaka <iyo.todaka@gmail.com>
2022-10-20 09:45:53 -0700

    ""

--- JS/WebDev2-W5-D1/assets/js/lab.js
+++ JS/WebDev2-W5-D1/assets/js/lab.js
@@ -1,7 +1,7 @@
 const TimeLine = gsap.timeline()
 
 TimeLine
-.from('.nav',{autoAlpha:0, diration:1, x: -50 })
+.from('.nav',{autoAlpha:0, diration:1, x: -50 ,stagger:0.08})
 .from('.title',{autoAlpha:0, diration:1, y: 30 })
 .from('.tagline',{autoAlpha:0, diration:1, y: 50 })
 .from('.desc',{autoAlpha:0, diration:1, y: 50 },"<")
