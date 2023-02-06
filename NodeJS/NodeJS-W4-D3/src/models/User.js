ec47c155f81cd116e58aff4ca03e746975841482
Iyo Todaka <iyo.todaka@gmail.com>
2023-02-06 04:50:45 -0800

    add comment

--- NodeJS/NodeJS-W4-D3/src/models/User.js
+++ NodeJS/NodeJS-W4-D3/src/models/User.js
@@ -11,8 +11,16 @@ const userSchema = new Schema({
     timestamps: true,
 })
 
+//mongoose.Schemaの中にpre・next・isModified関数がある
+//pre 第一引数に書かれている命令の前に第二引数の処理を実行させる
+//next() 次の処理へ進ませる↓の場合はnext()すると"save"が実行される
+//isModified 変更されたかどうか↓はパスワードが変更されていなければnext()を返す(次の処理に進む)
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
+
+    //bcryptの中にあるhashとsaltを使ってPWを暗号化させる
+    //hash 第一引数に暗号化したい文字、第二引数にハッシュ化させる回数もしくはオプション(saltとか)
+    //saltは回数ではなくランダムに生成された値、PWに追加する事でより解読を困難にする
     const hash = await bcrypt.hash(this.password, salt)
     this.password = hash
     next()
--- NodeJS/NodeJS-W4-D3/src/utils/db.js
+++ NodeJS/NodeJS-W4-D3/src/utils/db.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose')
+
+//DB接続 mongoDBのDatebase-Connect to Cluster0のconnect your appのURLをコピーして貼る
+//URLの一部にはPWを入力する、URL自体は.envで管理する
 const connect = mongoose.connect(process.env.MONGODB_URI)
 connect.then(() => console.log("Connected to DB")).catch(err => console.error(err))
