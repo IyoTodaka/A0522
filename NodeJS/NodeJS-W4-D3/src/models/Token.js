ae1bbade3a80ac8160ae6c316ea206dd2d8a7f74
Iyo Todaka <iyo.todaka@gmail.com>
2023-02-08 04:00:06 -0800

    add comment

--- NodeJS/NodeJS-W4-D3/src/models/Token.js
+++ NodeJS/NodeJS-W4-D3/src/models/Token.js
@@ -4,6 +4,9 @@ const { Schema, model, SchemaTypes } = mongoose
 const tokenSchema = new Schema({
     userId : { type: SchemaTypes.ObjectId, required: true, ref: "User" },
     token : { type: String, required: true },
+    //expires: 900　で900秒後にトークンの削除を行う
+    //トークンはサーバーにアクセスするための割符みたいなもの、ログイン時にレスポンスと一緒にユーザーに
+    //返されクッキーの中に保持される
     createdAt : { type: Date, required: true, default: () => Date.now(), expires: 900 }
 })
 
